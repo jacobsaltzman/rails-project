e59621865f4b57db2665b6bf3ef8b8258c24ca6f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-17 01:00:02 -0800

    Adds: table data for the migration, including references to the foreign keys.

--- db/migrate/20230117085546_create_loans.rb
+++ db/migrate/20230117085546_create_loans.rb
@@ -1,7 +1,11 @@
 class CreateLoans < ActiveRecord::Migration[6.1]
   def change
     create_table :loans do |t|
-
+      t.references :member, foreign_key: true
+      t.references :instrument, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+      t.string :status
       t.timestamps
     end
   end
