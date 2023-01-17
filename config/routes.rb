46ee1bab345be9ad8689d7b01d627d4355274b15
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-17 00:54:20 -0800

    Feat: generates the resources for the member model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :members
   resources :instruments
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
