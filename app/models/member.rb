456acf33ba00f16cd46c2cee7a507a9029cc907e
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-17 23:54:44 -0800

    Adds: relationships and validations for the member model.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,7 @@
 class Member < ApplicationRecord
+  has_many :loans
+  has_many :instruments, through: :loans
+  validates :username, uniqueness: true, presence: true
+  validates :password, presence: true 
+  has_secure_password
 end
