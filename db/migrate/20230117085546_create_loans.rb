f586ecfa60766da935076848294d31d1662279c0
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-17 23:41:46 -0800

    Fix: Changes references to belongs_to and adds the null false setting.

--- db/migrate/20230117085546_create_loans.rb
+++ db/migrate/20230117085546_create_loans.rb
@@ -1,8 +1,8 @@
 class CreateLoans < ActiveRecord::Migration[6.1]
   def change
     create_table :loans do |t|
-      t.references :member, foreign_key: true
-      t.references :instrument, foreign_key: true
+      t.belongs_to :member, null: false, foreign_key: true
+      t.belongs_to :instrument, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.string :status
