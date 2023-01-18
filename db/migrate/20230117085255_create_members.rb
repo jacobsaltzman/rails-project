cfc2e632439ec9c861182a5101fba14404cd7a83
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-17 23:39:08 -0800

    Fixes: changes password to password_digest.

--- db/migrate/20230117085255_create_members.rb
+++ db/migrate/20230117085255_create_members.rb
@@ -2,8 +2,8 @@ class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :members do |t|
       t.string :username
-      t.string :password
-
+      t.string :password_digest
+      t.string :email
       t.timestamps
     end
   end
