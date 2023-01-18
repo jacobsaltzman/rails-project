4c1f9408079edef0830acbbcc020c6c00ecfec59
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-17 23:14:30 -0800

    Removes brand in the table data, because it seems unnecessary at this point in the build.

--- db/migrate/20230117084807_create_instruments.rb
+++ db/migrate/20230117084807_create_instruments.rb
@@ -2,10 +2,8 @@ class CreateInstruments < ActiveRecord::Migration[6.1]
   def change
     create_table :instruments do |t|
       t.string :name
-      t.string :brand
       t.string :condition
       t.string :image
-
       t.timestamps
     end
   end
