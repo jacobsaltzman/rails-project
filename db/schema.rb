903968171811f2949c32febde9859a6b52411cb6
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-18 00:30:22 -0800

    Changes: creates a new migration that changes the schema to add a description column for the instrument table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_085546) do
+ActiveRecord::Schema.define(version: 2023_01_18_082825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_01_17_085546) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "loans", force: :cascade do |t|
