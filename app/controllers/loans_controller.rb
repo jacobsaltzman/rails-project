634de39216faa42b1bbca780a81a98bc08929d24
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-21 16:24:18 -0800

    Removes: index method and adds show method for returning individual loans.

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,2 +1,12 @@
 class LoansController < ApplicationController
+
+ 
+  # GET /loans/:id
+  #return individual instance of a loan
+  def show
+    loan = Loan.find(params[:id])
+    render json: loan, status: :ok
+  end
+
+
 end
