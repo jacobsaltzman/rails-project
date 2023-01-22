eff444e13789394dbda488d859756fc4e1f2f5cb
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-21 16:25:06 -0800

    Changes: resources for loans now limited to show and create, and resources for instruments expanded to full crud.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :loans, only: [ :index, :show, :create, :update, :destroy]
+  resources :loans, only: [:show, :create]
   resources :members, only: [:show, :create]
-  resources :instruments, only: [:show, :create]
+  resources :instruments, only: [ :index, :show, :create, :update, :destroy]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
