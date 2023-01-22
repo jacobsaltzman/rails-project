01bcd22cd89ab5cfec5c7281ce68cefadc580b27
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-21 16:23:12 -0800

    Adds: index method for returning all instruments.

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,9 @@
 class InstrumentsController < ApplicationController
+
+ # GET /instruments
+  # return all of the instruments
+  def index
+    render json: Instrument.all, status: :ok
+  end
+
 end
