619be3c4d52205f39353d99c0891eb7164cb720a
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-24 00:31:27 -0800

    Adds: baseline show and create actions for the member controller.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,20 @@
 class MembersController < ApplicationController
+
+  def show
+    user = Member.find(params[:id])
+    render json: user, status: :ok
+  end
+
+  def create
+    user = Member.create(member_params)
+    session[:member_id] = user.id
+    render json: user, status: :ok
+  end
+
+
+  private
+
+  def member_params
+    params.permit(:username, :password, :email)
+  end
 end
