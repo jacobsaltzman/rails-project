768aba91e61f4c71ff23e7418bec0a81bb6a11aa
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-24 00:25:41 -0800

    Adds: create action in the loan controller as well as defines the permitted params in the private method.

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,11 +1,20 @@
 class LoansController < ApplicationController
 
- 
-  # GET /loans/:id
-  #return individual instance of a loan
   def show
     loan = Loan.find(params[:id])
     render json: loan, status: :ok
+  end
+
+  def create
+    loan = Loan.create(loan_params)
+    render json: loan, status: :created
+  end
+
+
+  private
+
+  def loan_params
+    params.permit(:member_id, :instrument_id, :start_date, :end_date, :status)
   end
 
 
