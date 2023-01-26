162b37c67d7b17dee1d166a330950ec06691f7ca
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-26 02:58:02 -0800

    Adds: the useState and useEffect and puts them to use grabbing the instruments from the API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
 
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+
+  const [instruments, setInstruments] = useState([]);
+  const [errors, setErrors] = useState(false);
+  //const [currentUser, setCurrentUser] = useState(false);
+
+
+useEffect(()=> {
+  fetch("/instruments")
+  .then(res => {
+    if(res.ok){
+      res.json()
+      .then((data) =>{
+        setInstruments(data)
+      })
+    }else {
+      res.json().then(data => setErrors(data.error))
+    }
+  })
+  
+}, []);
+
+console.log(instruments, errors)
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
