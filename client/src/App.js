3925f356d77b80055ac3bfb54eb0f0e992a8b155
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-26 02:07:26 -0800

    Adds: a p element back in to make sure the testing environment is still rending content.

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <p> testing, testing</p>
       </header>
-      
+
       <Routes>
 
         <Route/>
