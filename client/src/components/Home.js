2556e12203f6e4df607adf4a41ae7e3f9787cf31
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-28 02:03:04 -0800

    Builds: the home section into a descriptive landing pad for new and old users.

---
+++ client/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home(){
+  return (
+    <div id="home">
+      <div id="home-top-div"> 
+      <p>Welcome to the Instrument Library</p>
+      </div>
+    <section id="home-middle-section">
+      <div id="main-logo-div">Well, hello, code explorer...</div>
+    </section>
+      <div id="home-bottom-div">
+      <h5>Browse the instrument collection, hear what others have to say about their experience, loan out an item, lend the library an instrument, or create your own profile and join the musical, happiness sharing community.</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
