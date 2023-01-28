88370847612b59ee1e4ec2f7819600d7a3c0818e
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-28 01:39:43 -0800

    Builds: the component footer out, ready to be exported.

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer(){
+
+  return(
+    <div id="footer">
+      <div id="by">
+      <p>Fullstack Application -- Rails (backend) and React (frontend) By:</p>
+      <a href="https://www.linkedin.com/in/jacob-saltzman/">Jacob Saltzman</a>
+      </div>
+    </div>
+  )
+
+}
+
+export default Footer;
