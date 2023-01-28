d2c152e3c953e099ce704a51ed6fe1616aaecfd9
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-27 19:03:51 -0800

    Changes: full crud operations over to the loans model from the instrument.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :loans, only: [:show, :create]
+  resources :loans, only: [:index, :show, :create, :update, :destroy]
   resources :members, only: [:show, :create]
-  resources :instruments, only: [ :index, :show, :create, :update, :destroy]
+  resources :instruments, only: [:index, :show, :create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
