9fd1412ecbbbefa6a9687980e6f2ad1f3c7a021b
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-01-30 02:33:14 -0800

    Feat: builds out sessions authentication and create and destroy method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
   def create
+    user = Member.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:member_id] = user.id
+      render json: user, status: :created
+
+    else
+      render json: "Invalid Credentials", status: :unauthorized 
+    end
+
+
   end
 
   def destroy
+    session.delete(:member_id)
   end
+  
 end
