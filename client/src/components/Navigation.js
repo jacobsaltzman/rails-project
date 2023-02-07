be5c0909416aaa029bebc1081b0a69d0ecf8078f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-07 01:08:44 -0800

    Styles: the login and signup links and merges the darkmode button into that div and sticks them up to the top right corner.

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -16,22 +16,19 @@ function Navigation( {currentUser, setCurrentUser, handleDarkMode, isDarkMode} )
     <header>
       
       <div className="navbar">
+        <div id="signin-signout">
+            {!currentUser ? <Link to="/login">Login</Link> : null}
+            {!currentUser ? <Link to="/signup">Signup</Link> : null}
+            {currentUser ? <div><h6>Welcome, {currentUser.username}</h6>   <button onClick={handleLogout}>Log Out</button> </div>: null}
+            <button onClick={handleDarkMode}>{isDarkMode? '☀️': '🌙'} </button>
+        </div>
+
         <div id="nav-links">
         <Link to="/">Home</Link>
         <Link to="/instruments">Instruments</Link>
         <Link to="/loans">Experiences</Link>
         <Link to={`/members/${currentUser.id}`}>Account</Link>
         <Link to="/about">About&nbsp;Us</Link>
-        </div>
-
-        <div id="signin-signout">
-          {!currentUser ? <Link to="/login">Login</Link> : null}
-          {!currentUser ? <Link to="/signup">Signup</Link> : null}
-          {currentUser ? <div><h6>Welcome, {currentUser.username}</h6>   <button onClick={handleLogout}>Log Out</button> </div>: null}
-        </div>
-
-        <div id="dark-button">
-          <button onClick={handleDarkMode}>{isDarkMode? 'Lightmode ☀️': 'Darkmode 🌙'} </button>
         </div>
 
       </div>
