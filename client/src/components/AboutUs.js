d35bbb8ed658137b862cb266388cbdad9b84d07c
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-07 22:49:23 -0800

    Changes: wording in the About us section, adding more details.

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -3,8 +3,11 @@ function AboutUs(){
   return(
     <div className= "about-us">
       <div><h3>What Is The Instrument Library Project?</h3></div>
-      <div> <p>The Instrument Library Project is a community powered platform for sharing the beautiful experience of playing new and exciting instruments. The cost barrier to getting into certain musical instruments can be outrageous. Even renting can prove to be incredibly costly in the long run. </p>
-      </div>
+      <div> <p>The Instrument Library Project is a community-powered platform for sharing the beautiful experience of playing new and exciting instruments. It functions like a library, wherein members can borrow instruments, but also like a social media platform, in which users can share their experience. </p></div>
+      <div> <h3>Why?</h3></div>
+      <div><p>The cost barrier for getting into certain musical instruments can be outrageous. Even renting can prove to be incredibly costly in the long run. We aim to provide free access to instruments so people can test, try, and practice on first, before making a decision to spend money on that potential hobby.</p></div>
+      
+  
     </div>
   )
 
