7bc4b28101b3647a15cfe71e72737599c02fa068
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-09 01:15:14 -0800

    Adds: routes for autologin functionality and for member signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get '/me', to: 'members#show'
+  post "/signup", to: "members#create"
 
 
   resources :loans, only: [:index, :show, :create, :update, :destroy]
   resources :members, only: [:show, :create]
   resources :instruments, only: [:index, :show, :create]
+
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
