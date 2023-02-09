9ba41c153a2b022fc4e5903f5459b168c744fe64
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-09 01:17:14 -0800

    Adds the errors read out to the nav bar.

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 
-function Navigation( {currentUser, setCurrentUser, handleDarkMode, isDarkMode} ){
+function Navigation( {errors, currentUser, setCurrentUser, handleDarkMode, isDarkMode} ){
 
   const handleLogout = () => {
     fetch('/logout', {
@@ -17,9 +17,11 @@ function Navigation( {currentUser, setCurrentUser, handleDarkMode, isDarkMode} )
       
       <div className="navbar">
         <div id="signin-signout">
+        
             {!currentUser ? <Link to="/login">Login</Link> : null}
             {!currentUser ? <Link to="/signup">Signup</Link> : null}
             <button onClick={handleDarkMode}>{isDarkMode? '☀️': '🌙'} </button>
+            {errors?<h6 id="errors">{errors}. Sign up or Login to continue!</h6>:""}
             {currentUser ? <div><div id="user-greeting"><h6>Welcome, {currentUser.username}</h6></div>  <div id="logout-button"><button onClick={handleLogout}>Log Out</button> </div></div>: null}
             
         </div>
