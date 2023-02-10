b29aa5011d4ed733ca83b5dbaffe616df9367090
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-10 01:13:22 -0800

    Changes: the find_by method, moving the username into the parameters of the method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
 
   def create
-    user = Member.find_by_username(params[:username])
+    user = Member.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:member_id] = user.id
       render json: user, status: :created
