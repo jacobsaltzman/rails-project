bda5d7a7034528112007835c8825575686863df1
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-10 02:17:04 -0800

    Styles: the status of each instrument and moves the details to the link component and removes the toggle for more info.

--- client/src/components/instrument/InstrumentCard.js
+++ client/src/components/instrument/InstrumentCard.js
@@ -1,30 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom'
 
 
 
 function InstrumentCard({instrument}){
 
-  const [isSeen, setIsSeen] = useState(true);
 
-  const { id, name, condition, description, image } = instrument;
+  const { id, name, condition, image } = instrument;
 
-
-  function handleIsSeen(e){
-    setIsSeen(!isSeen)
-  }
 
   return(
     <div className="instrument-card" id={id}>
-      <div className="background"></div>
       <h3>{name}</h3>
       <img alt={name} src={image}></img>
-      <h5>Current Status: {condition}</h5> 
-      <div>{isSeen?"Click to find out more.":<div><p>Information about the {name}: {description}</p><p> <Link to={`/instruments/${id}`}> Click here </Link> to check out other member's experiences with the {name}, loan it out, and more.</p></div>}</div>
-      <button onClick={handleIsSeen}>{isSeen? 'More Info':'Less info'}</button>
-      <div id='more-info'>
+      <div className="status">
+      <h5>Current Status:</h5>
+      <h6 className={condition}>{condition} </h6> 
       </div>
-      <div className="background"></div>
+      <div id='more-info'>
+      <Link to={`/instruments/${id}`}> More Info </Link>
+      </div>
+      
     </div>
   )
 }
