03a0ea0f5d8cafded98fb428af9fd64841d0296c
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-11 02:53:54 -0800

    Testing: comments and deploy success or failure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -89,56 +89,56 @@ puts "🌱 Seeding Instrument Library..."
    }
  ]
 
- user_data = [
-   {
-     username: "Admin",
-     password: "strongpassword",
-     email: "admin@library.com"
-   },
-   {
-     username: "Jacob",
-     password: "1111",
-     email: "railsdev@google.com"
-   },
-   {
-     username: "Nikki",
-     password: "2222",
-     email: "2spheres@google.com"
-   }
- ]
+  user_data = [
+    {
+      username: "Admin",
+      password: "strongpassword",
+      email: "admin@library.com"
+    },
+    {
+      username: "Jacob",
+      password: "1111",
+      email: "railsdev@google.com"
+    },
+    {
+      username: "Nikki",
+      password: "2222",
+      email: "2spheres@google.com"
+    }
+  ]
 
- loan_data = [
-   {
-     instrument_id: 2,
-     member_id: 1,
-     returned: true,
-     experience: 'It was great'
-   },
-   {
-    instrument_id: 4,
-    member_id: 1,
-    returned: true,
-    experience: 'It was okay'
-  },
-   {
-     instrument_id: 1,
-     member_id: 2,
-     returned: true,
-     experience: 'So challenging!'
-   },
-   {
-    instrument_id: 4,
-    member_id: 2,
-    returned: true,
-    experience: 'So much fun!'
-  },
-  {
-    instrument_id: 7,
-    member_id: 2,
-    returned: true,
-    experience: 'Not for me...'
-  }
- ]
+#  loan_data = [
+#    {
+#      instrument_id: 2,
+#      member_id: 1,
+#      returned: true,
+#      experience: 'It was great'
+#    },
+#    {
+#     instrument_id: 4,
+#     member_id: 1,
+#     returned: true,
+#     experience: 'It was okay'
+#   },
+#    {
+#      instrument_id: 1,
+#      member_id: 2,
+#      returned: true,
+#      experience: 'So challenging!'
+#    },
+#    {
+#     instrument_id: 4,
+#     member_id: 2,
+#     returned: true,
+#     experience: 'So much fun!'
+#   },
+#   {
+#     instrument_id: 7,
+#     member_id: 2,
+#     returned: true,
+#     experience: 'Not for me...'
+#   }
+#  ]
 
 
  user_data.each do |attr|
