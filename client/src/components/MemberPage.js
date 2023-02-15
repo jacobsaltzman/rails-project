e941d3704bb3921739519917b1fefeb6a6119803
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-15 00:35:01 -0800

    Adds: text that renders how many instruments the user has borrowed.

--- client/src/components/MemberPage.js
+++ client/src/components/MemberPage.js
@@ -4,9 +4,11 @@
 function MemberPage({currentUser}){
 
   let userCreationDate;
+  let instrumentNum;
 
-   if (currentUser.created_at){
+   if (currentUser){
       userCreationDate = currentUser.created_at.split('T')
+      instrumentNum = currentUser.instruments.length
    }
 
 
@@ -15,6 +17,7 @@ function MemberPage({currentUser}){
     <div>
     <h3>{currentUser.username}</h3>
     {userCreationDate?<p>Member since {userCreationDate[0]}</p>: ""}
+    {instrumentNum === 1 ? <p>You have loaned out {instrumentNum} instrument during your time as a member. Keep going! </p>:<p>You have loaned out {instrumentNum} instruments during your time as a member.</p>}
 
     <h5>Current Loans</h5>
     <p>Loans with a falsey return value here.</p>
