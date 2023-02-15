a6ed1b22305a47c29297ddaadd8487d2d873ea71
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-15 00:06:42 -0800

    Adds: update_at attribute for loan serializer details.

--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,4 +1,4 @@
 class LoanSerializer < ActiveModel::Serializer
-  attributes :id, :member_id, :instrument_id, :experience, :returned
+  attributes :id, :member_id, :instrument_id, :experience, :returned, :updated_at
   belongs_to :member
 end
