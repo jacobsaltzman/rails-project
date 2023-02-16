809fb1634abe12446c6c7794c8d4c269b1b7b87a
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-16 01:59:29 -0800

    Creates: the loan creation functionality, first creating a loan and then updating the status of the instrument.

--- client/src/components/loans/LoanCreation.js
+++ client/src/components/loans/LoanCreation.js
@@ -2,15 +2,57 @@ import React from 'react'
 
 function LoanCreation ({id, currentUser}){
 
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  console.log(id)
-  console.log(currentUser)
+
+    fetch('/loans', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify({ instrument_id: id, member_id: currentUser.id })
+    })
+    .then(response => {
+      if (response.ok) {
+        //console.log(response)
+        //loan resp will need to go into currentUser array
+        return fetch(`/instruments/${id}`, {
+          method: 'PATCH',
+          body: JSON.stringify({ condition: 'Loaned Out' }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      } else {
+        throw new Error('Failed to create loan');
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        //console.log(response)
+        //resp will prob need to go into the instruments array.
+        //will the updated array with "Available" gone be enough to disappear the borrow button? 
+        //Maybe should nav to account afterwards?
+      } else {
+        throw new Error('Failed to update instrument status');
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      //need to append any errors into html elements
+    });
+  };
 
 
     return (
       <div className='loan-creation'>
-        <p>This instrument is available for borrowing.</p>
+
+        <p>This instrument is available for loan.</p>
+        <p>Click "borrow" and pick up the instrument within 2 days to start playing.</p>
         
+        <button type="button" onClick={handleSubmit}>Borrow!</button>
 
       
       </div>
