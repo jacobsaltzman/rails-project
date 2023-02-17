3b2ab15e0b124be81a58db43b5830fd7c00c5315
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-16 22:07:59 -0800

    Adds: props for the setErrors function.

--- client/src/components/instrument/Instruments.js
+++ client/src/components/instrument/Instruments.js
@@ -3,7 +3,7 @@ import InstrumentContainer from "./InstrumentContainer";
 import InstrumentCreate from "./InstrumentCreate";
 
 
-function Instruments({ instruments, onAddInstrument }){
+function Instruments({ instruments, onAddInstrument, setErrors }){
 
   const [isSeen, setIsSeen] = useState(true);
 
@@ -15,7 +15,7 @@ function Instruments({ instruments, onAddInstrument }){
   return(
     <div id='main-instrument-list'>
       <button onClick={handleIsSeen}>{isSeen? 'Donate Now!': 'Nevermind'}</button>
-      {isSeen? <div></div>: <InstrumentCreate onAddInstrument={onAddInstrument}/>}
+      {isSeen? <div></div>: <InstrumentCreate setErrors={setErrors} onAddInstrument={onAddInstrument}/>}
       <h2>The comprehensive list of Instruments.</h2>
       <InstrumentContainer instruments={instruments}/>
     </div>
