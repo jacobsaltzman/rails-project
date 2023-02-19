d3aacbb8dfc9ed87111c8a76d8f0dc5780c2f85e
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-18 19:37:37 -0800

    Styles: the instrument page image and layout.

--- client/src/components/instrument/InstrumentPage.js
+++ client/src/components/instrument/InstrumentPage.js
@@ -18,14 +18,20 @@ function InstrumentPage({instruments, currentUser, onEditInstrument, setErrors, 
   
     return (
       <div className="individual-instrument-page">
-        <h3>{name}</h3>
+        <div className="image-container">
         <img alt="Instrument" src={image}></img>
+        </div>
+
+        <h3>{name}</h3>
+        <div id="instrument-details">
+        <h6>Details:</h6> <p>{description}</p>
         <h5>Status: {condition}</h5> 
         {condition === "Available"? <LoanCreation onAddLoan={onAddLoan} setErrors={setErrors} id={id} currentUser={currentUser} onEditInstrument={onEditInstrument}/>:<h6>Not available for borrow. Check back later.</h6>}
-        <h6>Details:</h6> <p>{description}</p>
-  
+        </div>
+        
 
         <div id="individual-instrument-experiences">
+        
         <h4>Read the Experiences:</h4>
         {loanArr && loanArr.length !==0? loanArr.map((loan) =>  
         <ul>
