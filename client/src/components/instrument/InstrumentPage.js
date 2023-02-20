eedaeb4a8ec60cc173111675bd7fbde852391a98
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 11:45:45 -0800

    Styles: some changes to the layout of elements.

--- client/src/components/instrument/InstrumentPage.js
+++ client/src/components/instrument/InstrumentPage.js
@@ -18,20 +18,25 @@ function InstrumentPage({instruments, currentUser, onEditInstrument, setErrors, 
   
     return (
       <div className="individual-instrument-page">
+
+        <div id="instrument-status">
+        <h5>Status: {condition}</h5> 
+        {condition === "Available"? <LoanCreation onAddLoan={onAddLoan} setErrors={setErrors} id={id} currentUser={currentUser} onEditInstrument={onEditInstrument}/>:<h6>Not available for borrow. Check back later.</h6>}
+        </div>
+
+        <h3>{name}</h3>
+        
         <div className="image-container">
         <img alt="Instrument" src={image}></img>
         </div>
 
-        <h3>{name}</h3>
+        
         <div id="instrument-details">
         <h6>Details:</h6> <p>{description}</p>
-        <h5>Status: {condition}</h5> 
-        {condition === "Available"? <LoanCreation onAddLoan={onAddLoan} setErrors={setErrors} id={id} currentUser={currentUser} onEditInstrument={onEditInstrument}/>:<h6>Not available for borrow. Check back later.</h6>}
         </div>
         
 
         <div id="individual-instrument-experiences">
-        
         <h4>Read the Experiences:</h4>
         {loanArr && loanArr.length !==0? loanArr.map((loan) =>  
         <ul>
@@ -39,6 +44,7 @@ function InstrumentPage({instruments, currentUser, onEditInstrument, setErrors, 
         </ul>)
         :"No loan history."}
         </div>
+
       </div>
     );
   } else {
