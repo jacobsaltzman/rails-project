6ee46c937a1b577187206f68a4c3705398034371
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 14:43:10 -0800

    Prepares: the passing of returned and current loan props to new components.

--- client/src/components/loans/LoansContainer.js
+++ client/src/components/loans/LoansContainer.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
 function LoansContainer ({currentUser}){
 
-    console.log(currentUser)
+  const { loans } = currentUser;
+
+
+  const [currentLoans, setCurrentLoans] = useState([]);
+  const [returnedLoans, setReturnedLoans] = useState([]);
+    
+  useEffect(() => {
+    if(loans){
+    const current = loans.filter((loan) => !loan.returned);
+    const returned = loans.filter((loan) => loan.returned);
+    setCurrentLoans(current);
+    setReturnedLoans(returned);
+    }
+  }, [loans]);
+
+  console.log(currentLoans)
+  console.log(returnedLoans)
 
     return (
-      <div>LoansContainer</div>
+      <div id='user-loans'>
+
+        <div id='current-loans'>
+
+        </div>
+
+
+        <div id='previous-loans'>
+          
+        </div>
+
+
+      </div>
     )
   
 }
