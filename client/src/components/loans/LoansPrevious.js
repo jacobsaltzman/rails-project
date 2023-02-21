898bb9b7ad64805e73ece19667168510316b4d5f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 19:10:46 -0800

    Adds: a rendering of the experience, loan origination date, and updated date.

--- client/src/components/loans/LoansPrevious.js
+++ client/src/components/loans/LoansPrevious.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
 
 export default function LoansPrevious( { returnedLoans }) {
 
+  const [isEditMode, setIsEditMode] = useState(false);
+  //const [editLoanId, setEditLoanId] = useState(null);
+
+
   console.log(returnedLoans)
 
-  function editLoan(x){
-    console.log(x)
+  function handleEditMode(){
+    setIsEditMode(!isEditMode)
   }
-
 
   return (
     <div id='loans-previous'>
       <h5>Previous Loans:</h5>
       {returnedLoans.map((loan) => (
-  <div key={loan.id}>
-    {loan.experience} 
-    <button onClick={() => editLoan(loan.id)}>Edit</button>
-  </div>
-))}
-    </div>
+      <div className='loan-history' key={loan.id}>
+        <h5>{loan.instrumentname}</h5>
+        <p>borrowed on: {loan.loan_began}</p>
+        <p className='loan-experience'>your experience: "{loan.experience}"</p>
+        <button onClick={() => handleEditMode()}>Edit</button>
+        <p className='loan-updated'>updated on: {loan.loan_updated}</p>
+        
+      </div>
+    ))}
+      </div>
   )
 }
