c7e53cd2d47bed82b3ee3b44f68f45dd6ac09a06
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 19:55:24 -0800

    Styles: major changes to the instrument cards, wrapping each as a link and getting rid of the more info button.

--- client/src/components/instrument/InstrumentCard.js
+++ client/src/components/instrument/InstrumentCard.js
@@ -10,19 +10,19 @@ function InstrumentCard({instrument}){
 
 
   return(
-    <div className="instrument-card" id={id}>
-      
+   
+      <Link to={`/instruments/${id}`} className="instrument-card" id={id}>
+ 
       <img alt={name} src={image}></img>
-      <h3>{name}</h3>
+      <h3 className='instrument-name'>{name}</h3>
       <div className="status">
-      <h5>Current Status:</h5>
+      <h5 className='instrument-status'>Current Status:</h5>
       <h6 className={condition}>{condition} </h6> 
       </div>
-      <div id='more-info'>
-      <Link to={`/instruments/${id}`}> More Info </Link>
-      </div>
       
-    </div>
+      </Link>
+      
+  
   )
 }
 
