1b8bedfe6532223f3df336c858fefc4a3cd2aab8
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-21 15:06:05 -0800

    Refactors: the code in the member page and serializer to be more streamlined and adds the date conversion to the backend.

--- app/serializers/member_serializer.rb
+++ app/serializers/member_serializer.rb
@@ -1,4 +1,9 @@
 class MemberSerializer < ActiveModel::Serializer
-  attributes :id, :username, :created_at
+  attributes :id, :username, :creationDate
   has_many :loans
+
+  def creationDate
+    object.created_at.strftime("%Y-%m-%d")
+  end
+
 end
--- client/src/components/MemberPage.js
+++ client/src/components/MemberPage.js
@@ -1,30 +1,14 @@
-//import { useEffect, useState } from 'react'
+//import { useState } from 'react'
 //import {useParams} from 'react-router-dom'
 import LoansContainer from "./loans/LoansContainer";
 
 function MemberPage({currentUser}){
 
-  let userCreationDate;
-  let instrumentNum;
-
-   if (currentUser){
-      if(currentUser.created_at !== undefined){
-      userCreationDate = currentUser.created_at.split('T')
-      instrumentNum = currentUser.loans.length
-      }
-      else{
-        userCreationDate = false;
-        instrumentNum = false;
-      }
-   }
-   
 
   return(
-    <div>
+    <div id='current-user-header'>
     <h3>{currentUser.username}</h3>
-    {!userCreationDate ? <p>Thanks for being a member.</p> : <p>Member since {userCreationDate[0]}</p>}
-    {!instrumentNum ? <p> View the instruments page and borrow a loan to start a new experience. </p> : <p>You have loaned out {instrumentNum} instruments during your time as a member.</p>}
-
+    {!currentUser.creationDate ? <p>Thanks for being a member.</p> : <p>Member since {currentUser.creationDate}</p>}
     
     <LoansContainer currentUser={currentUser} />
     </div>
