a8a1f89a14f358e2218da254e27e6cd9f083f01f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 17:17:15 -0800

    Adds: more attributes to be passed in the serializer, instrument, instr_id, and created_on.

--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,8 +1,13 @@
 class LoanSerializer < ActiveModel::Serializer
-  attributes :id, :experience, :returned, :updated_at, :name
+  attributes :id, :experience, :returned, :updated_at, :name, :instrumentname, :instrument_id, :created_at
   belongs_to :member, serializer: MemberSerializer
 
   def name
     object.member.username
   end
+
+  def instrumentname
+    object.instrument.name
+  end
+
 end
