34fcd65eef17c6281b521a6bf5fef506e8269ab1
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 19:07:08 -0800

    Changes: the updated_at and created_at displays with custom methods to render only formatted dates and times.

--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,5 +1,5 @@
 class LoanSerializer < ActiveModel::Serializer
-  attributes :id, :experience, :returned, :updated_at, :name, :instrumentname, :instrument_id, :created_at
+  attributes :id, :experience, :returned, :loan_updated, :name, :instrumentname, :instrument_id, :loan_began
   belongs_to :member, serializer: MemberSerializer
 
   def name
@@ -8,6 +8,14 @@ class LoanSerializer < ActiveModel::Serializer
 
   def instrumentname
     object.instrument.name
+  end
+
+  def loan_began
+    object.created_at.strftime("%Y-%m-%d")
+  end
+
+  def loan_updated
+    object.updated_at.strftime("%Y-%m-%d at %H:%M")
   end
 
 end
