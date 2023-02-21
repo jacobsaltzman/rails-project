6e1e0735c0a8aeffa2e54bcf5ecc61c05963678b
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2023-02-20 16:51:24 -0800

    Adds: map functions and buttons to each individual loan element.

--- client/src/components/loans/LoansCurrent.js
+++ client/src/components/loans/LoansCurrent.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 export default function LoansCurrent( {currentLoans} ) {
 
-  console.log(currentLoans)
-  
+  function returnLoan(x){
+  console.log(x)
+  }
+
+
   return (
-    <div>LoansCurrent</div>
+    <div id='loans-current'>
+      <h5>Current Loans:</h5>
+      {currentLoans.map((loan) => (
+      <div key={loan.id}>
+          {loan.experience} 
+          {loan.returned ? null : (
+          <button onClick={() => returnLoan(loan.id)}>Return</button>
+    )}
+  </div>
+))}
+
+    </div>
   )
 }
--- client/src/components/loans/LoansPrevious.js
+++ client/src/components/loans/LoansPrevious.js
@@ -4,8 +4,20 @@ export default function LoansPrevious( { returnedLoans }) {
 
   console.log(returnedLoans)
 
+  function editLoan(x){
+    console.log(x)
+  }
+
 
   return (
-    <div>LoansPrevious</div>
+    <div id='loans-previous'>
+      <h5>Previous Loans:</h5>
+      {returnedLoans.map((loan) => (
+  <div key={loan.id}>
+    {loan.experience} 
+    <button onClick={() => editLoan(loan.id)}>Edit</button>
+  </div>
+))}
+    </div>
   )
 }
